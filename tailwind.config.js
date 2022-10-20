0cbc4719c8f0e36e6d78c4681eccb27f878c076e
Adedamola Adeyemo <adeyemoadedamola1@gmail.com>
2022-10-20 14:39:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors:{
+        brightRed: 'hsl(12,88%,59%)',
+        darkBlue: 'hsl(228,39%,23%)',
+        veryLightGray: 'hsl(0,0%,98%)'
+      },
+      backgroundImage: {
+        'hero': "url('/images/1666122947 (1).jpg')"
+    } 
+  },
+  plugins: [],
+}}
