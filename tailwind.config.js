5d94b47eca7444f32ab4a10d0678f8c54f84d7f5
Adedamola Adeyemo <adeyemoadedamola1@gmail.com>
2022-10-28 17:31:19 +0100

    home page redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,16 @@ module.exports = {
     extend: {
       colors:{
         brightRed: 'hsl(12,88%,59%)',
-        darkBlue: 'hsl(228,39%,23%)',
+        charisBlue : '#219ebc',
+        lightCharisBlue: '#8ecae6',
+        darkBlue: '#023047',
         veryLightGray: 'hsl(0,0%,98%)'
       },
       backgroundImage: {
         'hero': "url('/images/1666122947 (1).jpg')"
     } 
   },
-  plugins: [],
-}}
+  plugins: [
+  ],
+}
+}
