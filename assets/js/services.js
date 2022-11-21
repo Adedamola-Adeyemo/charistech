1f465af17cb1a0a9c4bc9b885ca48cd0e6566f24
Adedamola Adeyemo <adeyemoadedamola1@gmail.com>
2022-11-21 14:51:46 +0100

    new pages added

---
+++ assets/js/services.js
@@ -0,0 +1,8 @@
+ // ===== Faq
+ const faqs = document.querySelectorAll(".single-faq");
+ faqs.forEach((el) => {
+   el.querySelector(".faq-btn").addEventListener("click", () => {
+     el.querySelector(".icon").classList.toggle("rotate-180");
+     el.querySelector(".faq-content").classList.toggle("hidden");
+   });
+ });
